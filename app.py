cf648f3cb9f5e5022386a94a8de9bc8cf4301589
mlfisch3 <38837568+mlfisch3@users.noreply.github.com>
2023-05-12 12:11:11 -0400

    app files

---
+++ app.py
@@ -0,0 +1,74 @@
+import streamlit as st
+import pandas as pd
+import json
+from datetime import datetime
+import os
+import re
+
+title = "GPTBox"
+st.set_page_config(page_title=title, layout="wide", initial_sidebar_state="expanded")
+
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: visible;}
+footer {visibility: hidden;}
+</style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+pd.options.display.width = 1000
+pd.options.display.max_colwidth = 9999
+
+# Load the JSON file
+@st.experimental_singleton()
+def load_data(conversations):
+    with open(conversations) as f:
+        data = json.load(f)
+    return data
+
+def extract_messages(mapping):
+    messages = []
+    for key, value in mapping.items():
+        message_data = value.get("message")
+        if message_data:
+            create_time = datetime.utcfromtimestamp(message_data.get("create_time")).strftime('%Y-%m-%d %H:%M:%S')
+            author = message_data.get("author", {}).get("role")
+            content = " ".join(message_data.get("content", {}).get("parts", []))
+            messages.append([create_time, author, content])
+    return messages
+
+import re
+
+def detect_latex(content):
+    # replace \(...\) with $...$
+    content = re.sub(r'\\\((.*?)\\\)', r'$\1$', content)
+    # replace $$...[] with $$...$$
+    content = re.sub(r'\$\$(.*?)\[\]', r'$$\1$$', content)
+    return content
+
+def main():
+
+    conversations = st.text_input("Chat History File (JSON):", "conversations.json")
+    if os.path.isfile(conversations):
+        data = load_data(conversations)
+
+        # Sidebar
+        st.sidebar.title("Conversations")
+        selected_conversation = st.sidebar.selectbox(
+            "Select a conversation", options=[item["title"] for item in data], index=0
+        )
+
+        # Main
+        st.title(selected_conversation)
+
+        for conversation in data:
+            if conversation["title"] == selected_conversation:
+                messages = extract_messages(conversation["mapping"])
+#                df = pd.DataFrame(messages, columns=["Timestamp", "Author", "Content"])
+#                st.table(df)
+                for message in messages:
+                    content = detect_latex(message[2])
+                    st.markdown(f"**{message[1]} at {message[0]}**: {content}")
+
+if __name__ == "__main__":
+    main()
